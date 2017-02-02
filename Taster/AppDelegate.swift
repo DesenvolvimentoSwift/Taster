d4a6747cc4cd89d18c6b191ff0cac65501a4a13d
catarinaipleiria <branco>
2017-02-02 19:50:23 +0000

    Added description for photo usage.

--- Taster/AppDelegate.swift
+++ Taster/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         // Inicialização antes de exibir a aplicação ao utilizador.
-        FoodRepository.repository.populate()
+        ///FoodRepository.repository.populate()
         
         return true
     }
